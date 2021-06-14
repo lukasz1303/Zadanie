5442e736b9a8f9cdfc9f90a0a857d71922f1df83
Łukasz Frątczak <lukasz_fratczak@wp.pl>
2021-06-14 11:44:21 +0200

    test49

--- watek_glowny.c
+++ watek_glowny.c
@@ -70,7 +70,7 @@ void mainLoop()
 				while (1) {
 					int c = 0;
 					for (int i = 0; i < size; i++) {
-						if (msg_received[i] > 0) {
+						if (msg_received[i] == 1 || msg_received[i] == 3) {
 							c++;
 						}
 					}
